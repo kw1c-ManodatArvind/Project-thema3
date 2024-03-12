a5333da10cd1fab7147a7759321245cadcf15b65
kw1c-ManodatArvind <arvind.manodat@edu-kw1c.nl>
2024-03-12 14:41:12 +0100

    index.php

--- index.php
+++ index.php
@@ -9,15 +9,15 @@
 <body>
 <header class="header">
     <div class="logo">
-        <img src="tui_logo.png" alt="TUI Logo">
+        <img src="../Project-thema3/images/Logo.jpg" alt="TUI Logo">
     </div>
     <div class="search-bar">
         <input type="text" placeholder="Zoeken...">
         <button>Zoeken</button>
     </div>
     <div class="links">
-        <a href="service_contact.php">Service & Contact</a>
-        <a href="tui_vragenlijst.php">TUI Vragenlijst</a>
+        <a href="../Project-thema3/Pages/ServiceContact.php">Service & Contact</a>
+        <a href="../Project-thema3/Pages/Vragenlijst.php">TUI Vragenlijst</a>
     </div>
 </header>
 <!-- Overige inhoud van de website komt hier -->
