17e6501cbfd35b4b299c8d94f093af161d43829c
kw1c-JovanovicDanijel <danijel.jovanovic@edu-kw1c.nl>
2024-03-12 15:10:15 +0100

    commit

--- index.php
+++ index.php
@@ -3,23 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TUI Website</title>
-    <link rel="stylesheet" href="../Project-thema3/styles/styles.css">
+    <title>Zoo Website</title>
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
-<header class="header">
-    <div class="logo">
-        <img src="../Project-thema3/images/Logo.jpg" alt="TUI Logo">
+<div class="navbar" style="background-color: #71cbf4;">
+    <img src="images/Logo.jpg" alt="Logo" class="logo">
+    <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Vakanties</a></li>
+        <li><a href="#">Cruises</a></li>
+        <li><a href="#">Service & Contact</a></li>
+        <li><a href="#">Vliegtickets</a></li>
+    </ul>
+</div>
+<img src="images/HeaderPlaatje.jpg" alt="Header Image" class="header-img">
+<div class="container">
+    <div class="square-images">
+        <img src="images/Familie.jpg" alt="Image 1">
+        <img src="images/Vrouw.jpg" alt="Image 2">
+        <img src="images/VrouwMetKind.jpg" alt="Image 3">
+        <img src="images/Logo.jpg" alt="Image 4">
     </div>
-    <div class="search-bar">
-        <input type="text" placeholder="Zoeken...">
-        <button>Zoeken</button>
+    <p class="description">Relaxed boeken met TUI Garanties en Verzekeringen.</p>
+    <div class="blue-boxes">
+        <div class="blue-box">Omruilgarantie</div>
+        <div class="blue-box">Geld-Terug-Garantie</div>
+        <div class="blue-box">Laagste prijs garantie</div>
+        <div class="blue-box">Repatriëringsgarantie</div>
     </div>
-    <div class="links">
-        <a href="../Project-thema3/Pages/ServiceContact.php">Service & Contact</a>
-        <a href="../Project-thema3/Pages/Vragenlijst.php">TUI Vragenlijst</a>
-    </div>
-</header>
-<!-- Overige inhoud van de website komt hier -->
+</div>
 </body>
 </html>
+
