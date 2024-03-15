5afb7c9f0194b45c16168be130d073b67cec6709
kw1c-ManodatArvind <arvind.manodat@edu-kw1c.nl>
2024-03-15 14:02:39 +0100

    J

--- Pages/ServiceContact.php
+++ Pages/ServiceContact.php
@@ -1,1 +1,53 @@
-<?php
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoo Website</title>
+    <link rel="stylesheet" href="../styles/ServiceContact.css">
+    <script src="../scripts/script.js" defer> </script>
+</head>
+<body>
+<div class="navbar" style="background-color: #71cbf4;">
+    <img src="../images/Logo.jpg" alt="Logo" class="logo">
+    <ul class="nav-links">
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="../index.php">Vakanties</a></li>
+        <li><a href="../Pages/Cruises.php">Cruises</a></li>
+        <li><a href="../Pages/ServiceContact.php">Service & Contact</a></li>
+        <li><a href="../Pages/Vliegtickets.php">Vliegtickets</a></li>
+    </ul>
+</div>
+<img src="../images/HeaderPlaatje.jpg" alt="Header Image" class="header-img">
+<div style="text-align: center; margin-top: 20px;">
+    <h1 style="color: navy; font-size: 36px;">Service & contact</h1>
+    <h1 style="color: navy; font-size: 36px;">Vraag het onze virtuele assistent </h1>
+</div>
+<div class="decorative-links-container">
+    <div class="decorative-links">
+        <h2>Zelf regelen in myTUI</h2>
+        <ul>
+            <li><a href="#">Mijn boekingen bekijken</a> <span class="arrow">&#10132;</span></li>
+            <li><a href="#">Mijn boekingen betalen</a> <span class="arrow">&#10132;</span></li>
+            <li><a href="#">E-tickets downloaden</a> <span class="arrow">&#10132;</span></li>
+        </ul>
+    </div>
+
+    <div class="decorative-links">
+        <h2>Zelf regelen TUI fly</h2>
+        <ul>
+            <li><a href="#">Online inchecken</a> <span class="arrow">&#10132;</span></li>
+            <li><a href="#">Stoel reserveren</a> <span class="arrow">&#10132;</span></li>
+            <li><a href="#">Bagage</a> <span class="arrow">&#10132;</span></li>
+        </ul>
+    </div>
+
+    <div class="decorative-links">
+        <h2>Extra bijboeken</h2>
+        <ul>
+            <li><a href="#">Verzekeringen</a> <span class="arrow">&#10132;</span></li>
+            <li><a href="#">Parkeren</a> <span class="arrow">&#10132;</span></li>
+            <li><a href="#">Schiphol Lounge</a> <span class="arrow">&#10132;</span></li>
+        </ul>
+    </div>
+</div>
