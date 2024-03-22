9a1eea092638b474e97698cc58f5b80901971dfd
kw1c-ManodatArvind <arvind.manodat@edu-kw1c.nl>
2024-03-22 12:14:58 +0100

    14257

--- Pages/Cruises.php
+++ Pages/Cruises.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zoo Website</title>
-    <link rel="stylesheet" href="../styles/cruises.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<div class="navbar" style="background-color: #71cbf4;">
+<div class="navbar">
     <img src="images/logo.png" alt="Logo" class="logo">
     <ul class="nav-links">
         <li><a href="index.php">Home</a></li>
@@ -16,5 +16,26 @@
         <li><a href="Pages/Vliegtickets.php">Vliegtickets</a></li>
         <li><a href="Pages/Vragenlijst.php">Vragenlijst</a></li>
     </ul>
+    <div class="social-icons">
+        <img src="../images/facebook.png" alt="Facebook">
+        <img src="../images/twitter.png" alt="Twitter">
+    </div>
 </div>
-<img src="../images/cruises.jpg" alt="Header Image" class="header-img">
+<div class="header">
+    <img src="../images/cruises.jpg" alt="Header Image" class="header-img">
+    <div class="header-text">Vind jouw ideale cruise</div>
+    <div class="cruise-link">Cruise</div>
+</div>
+<div class="content-section">
+    <div class="inner-content">
+        <img src="../images/Cruise.jpg" alt="Second Image">
+        <div class="inner-content-text">
+            <p>Vaar mee naar de mooiste bestemmingen.</p>
+            <p>Een cruise staat bij veel mensen op het verlanglijstje en dat is niet voor niets. Een cruiseschip is eigenlijk een drijvend resort met alle luxe en comfort. Elke dag geniet je van een nieuw spectaculair uitzicht.</p>
+            <p class="discount-text">Tot 100,- korting p.p.</p>
+            <div class="red-button">Bekijk alle cruises</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
