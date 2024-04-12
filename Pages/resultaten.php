fb4794b0a6a023a79c6dfc808e01b0fc7a519397
kw1c-ManodatArvind <arvind.manodat@edu-kw1c.nl>
2024-04-12 09:02:57 +0200

    Commentaar bijgewerkt

--- Pages/resultaten.php
+++ Pages/resultaten.php
@@ -59,7 +59,7 @@ Datum: 9-4-2024
         "Thailand" => range(61, 81),
         "Verenigde Staten" => range(82, 102),
         "Kenia" => range(103, 123),
-        "Australië" => range(124, 144),
+
 
     );
 
@@ -68,7 +68,7 @@ Datum: 9-4-2024
         "Thailand" => "../images/thailand.jpg",
         "Verenigde Staten" => "../images/Usa.jpg",
         "Kenia" => "../images/Kenia.jpg",
-        "Australië" => "../images/Australie.jpg",
+
 
     );
 
@@ -77,7 +77,7 @@ Datum: 9-4-2024
         "Thailand" => "https://www.thuisvaccinatie.nl/bestemmingen/thailand/praktische-info/?msclkid=0c68fb78ddb01887a81e9d772d989fc6&utm_source=bing&utm_medium=cpc&utm_campaign=EFF%20-%20DSA&utm_term=Bestemmingen&utm_content=Bestemmingen",
         "Verenigde Staten" => "https://www.nederlandwereldwijd.nl/reisadvies/verenigde-staten-van-amerika",
         "Kenia" => "https://www.tourcompass.nl/kenia/reizen.htm?msclkid=9ef81f4ce7181c993c5b7943161a8b60&utm_source=bing&utm_medium=cpc&utm_campaign=1%20AFRICA%3A%20Kenya&utm_term=%2Bkenia%20%2Brondreis&utm_content=Kenia%20Rondreis",
-        "Australië" => "https://www.tourcompass.nl/australie/reizen/hoogtepunten-van-australie-met-sydney-uluru-en-cairns.htm?msclkid=096b1b439e9b1d42a4f17d4c6fc358f6&utm_source=bing&utm_medium=cpc&utm_campaign=4%20OCEAN%3A%20Australia&utm_term=australi%C3%AB%20reizen&utm_content=Australi%C3%AB%20reizen.",
+
 
     );
 
@@ -102,10 +102,6 @@ Datum: 9-4-2024
                         case "Afrika":
                             $punten = 103;
                             break;
-                        case "Oceanie":
-                            $punten = 124;
-                            break;
-                        // Voeg hier andere continenten en bijbehorende punten toe
                         default:
                             $punten = 0; // Standaardwaarde voor ongeldige antwoorden
                     }
